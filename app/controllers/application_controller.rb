d7c53b64459cd609547f0083e4aca72655200a72
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-11 19:04:08 +0300

    controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
+  get "/movies" do
     all_movies = Movie.all
     all_movies.to_json
   end
@@ -10,6 +10,95 @@ class ApplicationController < Sinatra::Base
   get './movies/:id' do
     movies = Movie.find(params[:id])
     movies.to_json
+  end
+
+  get './reviews/:id' do
+    reviews = Review.find(params[:id])
+    reviews.to_json
+  end
+
+  get './users/:id' do
+    users = User.find(params[:id])
+    users.to_json
+  end
+
+  post '/movies' do
+    movie = Movie.create(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+      rating: params[:rating]
+    )
+    movie.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      title: params[:title],
+      content: params[:content],
+      rating: params[:rating],
+      movie_id: params[:movie_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  post '/users' do
+    user = User.create(
+      username: params[:username],
+      password_digest: params[:password_digest]
+    )
+    user.to_json
+  end
+
+  patch '/movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.update(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+      rating: params[:rating]
+    )
+    movie.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      title: params[:title],
+      content: params[:content],
+      rating: params[:rating],
+      movie_id: params[:movie_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/users/:id' do
+    user = User.find(params[:id])
+    user.update(
+      username: params[:username],
+      password_digest: params[:password_digest]
+    )
+    user.to_json
+  end
+
+  delete '/movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.destroy
+    movie.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  delete '/users/:id' do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
   end
 
 end
