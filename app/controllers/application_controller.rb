0ea6f450674b34cb5a0ef3c647d660f804dcb534
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-18 18:00:25 +0300

    update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,39 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+
+  #GET /TvShows
+
+  get '/tv_shows' do
+      all_tv_shows = TvShow.all
+      all_tv_shows.to_json
+  end
+
+  get '/tv_shows/:id' do
+      tv_shows = TvShow.find(params[:id])
+      tv_shows.to_json
+  end
+
+  get '/episodes' do
+      all_episodes = Episode.all
+      all_episodes.to_json
+  end
+
+  get '/episodes/:id' do
+      episodes = Episode.find(params[:id])
+      episodes.to_json
+  end
+
+  get '/characters' do
+      all_characters = Character.all
+      all_characters.to_json
+  end
+
+  get '/characters/:id' do
+      characters = Character.find(params[:id])
+      characters.to_json
+  end
+  # GET /Movies
   get "/movies" do
     all_movies = Movie.all
     all_movies.to_json
@@ -32,6 +64,41 @@ class ApplicationController < Sinatra::Base
     users.to_json
   end
 
+  # POST /TvShows
+
+  post '/tv_shows' do
+      tv_show = TvShow.create(
+          title: params[:title],
+          image_url: params[:image_url],
+          year: params[:year],
+          plot: params[:plot],
+          rating: params[:rating]
+      )
+      tv_show.to_json
+  end
+
+  post '/episodes' do
+      episode = Episode.create(
+          title: params[:title],
+          image_url: params[:image_url],
+          year: params[:year],
+          plot: params[:plot],
+          rating: params[:rating],
+          tv_show_id: params[:tv_show_id]
+      )
+      episode.to_json
+  end
+
+  post '/characters' do
+      character = Character.create(
+          name: params[:name],
+          image_url: params[:image_url],
+          actor: params[:actor],
+          tv_show_id: params[:tv_show_id]
+      )
+      character.to_json
+  end
+
   post '/movies' do
     movie = Movie.create(
       title: params[:title],
@@ -60,6 +127,46 @@ class ApplicationController < Sinatra::Base
     )
     user.to_json
   end
+
+  # PATCH /TvShows
+
+  patch '/tv_shows/:id' do
+      tv_show = TvShow.find(params[:id])
+      tv_show.update(
+          title: params[:title],
+          image_url: params[:image_url],
+          year: params[:year],
+          plot: params[:plot],
+          rating: params[:rating]
+      )
+      tv_show.to_json
+  end
+
+  patch '/episodes/:id' do
+      episode = Episode.find(params[:id])
+      episode.update(
+          title: params[:title],
+          image_url: params[:image_url],
+          year: params[:year],
+          plot: params[:plot],
+          rating: params[:rating],
+          tv_show_id: params[:tv_show_id]
+      )
+      episode.to_json
+  end
+
+  patch '/characters/:id' do
+      character = Character.find(params[:id])
+      character.update(
+          name: params[:name],
+          image_url: params[:image_url],
+          actor: params[:actor],
+          tv_show_id: params[:tv_show_id]
+      )
+      character.to_json
+  end
+
+  # PATCH /Movies
 
   patch '/movies/:id' do
     movie = Movie.find(params[:id])
@@ -108,6 +215,28 @@ class ApplicationController < Sinatra::Base
     )
     user.to_json
   end
+
+  # DELETE /TvShows
+
+  delete '/tv_shows/:id' do
+      tv_show = TvShow.find(params[:id])
+      tv_show.destroy
+      tv_show.to_json
+  end
+
+  delete '/episodes/:id' do
+      episode = Episode.find(params[:id])
+      episode.destroy
+      episode.to_json
+  end
+
+  delete '/characters/:id' do
+      character = Character.find(params[:id])
+      character.destroy
+      character.to_json
+  end
+
+  # DELETE /Movies
 
   delete '/movies/:id' do
     movie = Movie.find(params[:id])
