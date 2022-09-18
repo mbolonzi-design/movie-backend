9c2aa943b6ed29bce58ca4a4f1c873aca2c355f2
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-18 16:15:49 +0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_11_100823) do
+ActiveRecord::Schema.define(version: 2022_09_18_063955) do
 
-  create_table "movies", force: :cascade do |t|
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "actor"
+    t.integer "age"
+    t.string "bio"
+    t.integer "tv_show_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "episodes", force: :cascade do |t|
     t.string "title"
+    t.string "image_url"
     t.integer "year"
     t.string "plot"
     t.integer "rating"
+    t.integer "tv_show_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.integer "year"
+    t.string "plot"
+    t.integer "rating"
+    t.string "review"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -27,6 +51,17 @@ ActiveRecord::Schema.define(version: 2022_09_11_100823) do
     t.string "rating"
     t.string "movie_id"
     t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tv_shows", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.integer "year"
+    t.string "plot"
+    t.integer "rating"
+    t.string "characters"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
